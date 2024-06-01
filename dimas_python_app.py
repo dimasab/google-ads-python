2169fc47ca96112eec2e62596233b9b0fc31ad12
Dimas <78547184+dimasab@users.noreply.github.com>
2024-06-01 19:07:25 +0700

    Update dimas_python_app.py

--- dimas_python_app.py
+++ dimas_python_app.py
@@ -1145,11 +1145,11 @@ def python_hapus_campaign_googleads(google_ads_customer_id, campaign_id):
 ###########################################################################################################################
 ###########################################################################################################################
 ######################################################## MULAI APP ########################################################
-def python_update_ad_googleads(google_ads_customer_id, ad_id, jenisproduk, merekproduk, namaproduk, spesifikasiproduk, hargaproduk, lokasitoko):
+def python_update_ad_googleads(google_ads_customer_id, ad_id, jenisproduk, merekproduk, namaproduk, spesifikasiproduk, hargaproduk, lokasitoko, urltarget):
     print("/python-update-ad-googleads terpanggil")
 
     # [START update_responsive_search_ad]
-    def main(client, customer_id, ad_id, jenisproduk, merekproduk, namaproduk, spesifikasiproduk, hargaproduk, lokasitoko):
+    def main(client, customer_id, ad_id, jenisproduk, merekproduk, namaproduk, spesifikasiproduk, hargaproduk, lokasitoko, urltarget):
         ad_service = client.get_service("AdService")
         ad_operation = client.get_type("AdOperation")
 
@@ -1214,8 +1214,8 @@ def python_update_ad_googleads(google_ads_customer_id, ad_id, jenisproduk, merek
             [description_1, description_2, description_3]
         )
 
-        ad.final_urls.append("https://www.example.com")
-        ad.final_mobile_urls.append("https://www.example.com/mobile")
+        ad.final_urls.append(urltarget)
+        # ad.final_mobile_urls.append(urltarget)
         client.copy_from(
             ad_operation.update_mask, protobuf_helpers.field_mask(None, ad._pb)
         )
@@ -1234,7 +1234,7 @@ def python_update_ad_googleads(google_ads_customer_id, ad_id, jenisproduk, merek
     if __name__ == "__main__":
         googleads_client = GoogleAdsClient.load_from_storage(path='./google-ads.yaml', version='v16')
         try:
-            main(googleads_client, google_ads_customer_id, ad_id, jenisproduk, merekproduk, namaproduk, spesifikasiproduk, hargaproduk, lokasitoko)
+            main(googleads_client, google_ads_customer_id, ad_id, jenisproduk, merekproduk, namaproduk, spesifikasiproduk, hargaproduk, lokasitoko, urltarget)
         except GoogleAdsException as ex:
             print(
                 f'Request with ID "{ex.request_id}" failed with status '
@@ -1503,6 +1503,201 @@ def python_update_keyword_googleads(google_ads_customer_id, id_adgroup, merekpro
 ###########################################################################################################################
 ###########################################################################################################################
 ######################################################## MULAI APP ########################################################
+def python_update_locations_googleads(google_ads_customer_id, id_kampanye, lokasitoko):
+
+    print("/python_update_locations_googleads terpanggil")
+
+    # Geo targeting from user.
+    GEO_LOCATION_1 = lokasitoko
+    # GEO_LOCATION_2 = "kabupaten bandung barat"
+    # GEO_LOCATION_3 = "kota bogor"
+
+    # LOCALE and COUNTRY_CODE are used for geo targeting.
+    # LOCALE is using ISO 639-1 format. If an invalid LOCALE is given,
+    # 'es' is used by default.
+    LOCALE = "id"
+
+    # A list of country codes can be referenced here:
+    # https://developers.google.com/google-ads/api/reference/data/geotargets
+    COUNTRY_CODE = "ID"
+
+    def baca_lokasi_sekarang(client, customer_id, campaign_resource_name):
+        """Fetches existing keywords in an ad group."""
+        ga_service = client.get_service("GoogleAdsService")
+
+        query = f"""
+            SELECT 
+                campaign_criterion.criterion_id,
+                campaign_criterion.location.geo_target_constant
+            FROM
+                campaign_criterion
+            WHERE
+                campaign_criterion.type = 'LOCATION' AND
+                campaign_criterion.campaign = '{campaign_resource_name}'
+        """
+
+        response = ga_service.search(customer_id=customer_id, query=query)
+        locations = [row.campaign_criterion.resource_name for row in response]
+
+        return locations
+
+    def hapus_lokasi_sekarang(client, customer_id, locations):
+        """Removes the specified locations."""
+        campaign_criterion_service = client.get_service("CampaignCriterionService")
+
+        operations = []
+        for location in locations:
+            operation = client.get_type("CampaignCriterionOperation")
+            operation.remove = location
+            operations.append(operation)
+
+        response = campaign_criterion_service.mutate_campaign_criteria(
+            customer_id=customer_id, operations=operations
+        )
+
+        for result in response.results:
+            print(f"Removed location {result.resource_name}.")
+
+    def pasang_lokasi_baru(client, customer_id, campaign_resource_name):
+        """Creates geo targets.
+
+        Args:
+        client: an initialized GoogleAdsClient instance.
+        customer_id: a client customer ID.
+        campaign_resource_name: an campaign resource name.
+
+        Returns:
+        Geo targets.
+        """
+        geo_target_constant_service = client.get_service("GeoTargetConstantService")
+
+        # Search by location names from
+        # GeoTargetConstantService.suggest_geo_target_constants() and directly
+        # apply GeoTargetConstant.resource_name.
+        gtc_request = client.get_type("SuggestGeoTargetConstantsRequest")
+        gtc_request.locale = LOCALE
+        gtc_request.country_code = COUNTRY_CODE
+
+        # The location names to get suggested geo target constants.
+        gtc_request.location_names.names.extend(
+            [
+             GEO_LOCATION_1, 
+            #  GEO_LOCATION_2, 
+            #  GEO_LOCATION_3
+             ]
+        )
+
+        results = geo_target_constant_service.suggest_geo_target_constants(
+            gtc_request
+        )
+
+        operations = []
+        for suggestion in results.geo_target_constant_suggestions:
+            print(
+                "geo_target_constant: "
+                f"{suggestion.geo_target_constant.resource_name} "
+                f"is found in LOCALE ({suggestion.locale}) "
+                f"with reach ({suggestion.reach}) "
+                f"from search term ({suggestion.search_term})."
+            )
+            # Create the campaign criterion for location targeting.
+            campaign_criterion_operation = client.get_type(
+                "CampaignCriterionOperation"
+            )
+            campaign_criterion = campaign_criterion_operation.create
+            campaign_criterion.campaign = campaign_resource_name
+            campaign_criterion.location.geo_target_constant = (
+                suggestion.geo_target_constant.resource_name
+            )
+            operations.append(campaign_criterion_operation)
+
+        campaign_criterion_service = client.get_service("CampaignCriterionService")
+        campaign_criterion_response = (
+            campaign_criterion_service.mutate_campaign_criteria(
+                customer_id=customer_id, operations=[*operations]
+            )
+        )
+
+        for result in campaign_criterion_response.results:
+            print(f'Added campaign criterion "{result.resource_name}".')
+
+    def main(client, customer_id, campaign_resource_name):
+        try:
+            # update_geo_targeting(client, customer_id, campaign_resource_name, lokasitoko)
+
+            # Fetch existing keywords
+            existing_locations = baca_lokasi_sekarang(client, customer_id, campaign_resource_name)
+
+            # Remove existing keywords
+            if existing_locations:
+                hapus_lokasi_sekarang(client, customer_id, existing_locations)
+
+            pasang_lokasi_baru(client, customer_id, campaign_resource_name)
+
+        except GoogleAdsException as ex:
+            print(f"Request with ID '{ex.request_id}' failed with status '{ex.error.code().name}' and includes the following errors:")
+            for error in ex.failure.errors:
+                print(f"\tError with message '{error.message}'.")
+                if error.location:
+                    for field_path_element in error.location.field_path_elements:
+                        print(f"\t\tOn field: {field_path_element.field_name}")
+
+    if __name__ == "__main__":
+        googleads_client = GoogleAdsClient.load_from_storage(path='./google-ads.yaml', version='v16')
+        campaign_resource_name = f"customers/{google_ads_customer_id}/campaigns/{id_kampanye}"
+        main(googleads_client, google_ads_customer_id, campaign_resource_name)
+######################################################## SELESAI APP ########################################################
+###########################################################################################################################
+###########################################################################################################################
+###########################################################################################################################
+###########################################################################################################################
+###########################################################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+###########################################################################################################################
+###########################################################################################################################
+###########################################################################################################################
+###########################################################################################################################
+###########################################################################################################################
+######################################################## MULAI APP ########################################################
 def cut_string(input_str, max_length=30):
     if len(input_str) <= max_length:
         return input_str
@@ -1667,8 +1862,9 @@ def semua():
             id_ad = str(product.get("acf")['google_ads_ad_id'])
 
             print(f'-Sudah ada campaign id, adgroup id, can ad id. Akan update produk di Campaign ID {id_kampanye}, Adgroup ID {id_adgroup}, dan Ad ID {id_ad}')
-            python_update_ad_googleads(google_ads_customer_id, id_ad, jenisproduk, merekproduk, namaproduk, spesifikasiproduk, hargaproduk, lokasitoko)
-            python_update_keyword_googleads(google_ads_customer_id, id_adgroup, merekproduk_cut, namaproduk_cut, spesifikasiproduk_cut)
+            python_update_ad_googleads(google_ads_customer_id, id_ad, jenisproduk, merekproduk, namaproduk, spesifikasiproduk, hargaproduk, lokasitoko, urltarget)
+            # python_update_keyword_googleads(google_ads_customer_id, id_adgroup, merekproduk_cut, namaproduk_cut, spesifikasiproduk_cut)
+            python_update_locations_googleads(google_ads_customer_id, id_kampanye, lokasitoko)
         else:
             #buat POST ke google ads untuk bikin campaign baru
             print('-Campaign id, adgroup id, atau ad id belum ada. Akan buat campaign baru untuk produk ID '+str(product.get("acf")['single_item_id']))
